7b35b294c3347c5cf6134f85f0538a24fcdee367
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-09-20 08:43:51 +0100

    Setup connection to db

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ModalProvider } from '@/providers/modal-provider'
+import prismadb from '@/lib/prismadb'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
+    return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -3,66 +3,94 @@
 import { useStoreModal } from "@/hooks/use-store-modal"
 import Modal from "@/components/ui/modal"
 import * as z from "zod"
+import axios from "axios"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
 
 const formSchema = z.object({
-    name: z.string().min(1),
+  name: z.string().min(1),
 })
 
 export const StoreModal = () => {
-    const storeModal = useStoreModal();
+  const storeModal = useStoreModal()
 
-    const [isLoading, setIsLoading] = useState(false);
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            name: "",
-        }
-    });
+  const [isLoading, setIsLoading] = useState(false)
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  })
 
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setIsLoading(true);
 
+      const response = await axios.post("/api/stores", values);
 
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+      console.log("[Data]",response.data);
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
     }
+  }
 
-    return (
-        <Modal
-            title="Create Store"
-            desc="New products and categories! Nice one baby girl"
-            isOpen={storeModal.isOpen}
-            onClose={storeModal.onClose}
-        >
-            <div>
-                <div className="space-y-4 py-2 pb-4">
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)}>
-                            <FormField
-                                control={form.control}
-                                name="name"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Store Name</FormLabel>
-                                        <FormControl>
-                                            <Input disabled={isLoading} placeholder="Shirts Store" {...field} />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                                <Button disabled={isLoading} variant={"outline"} onClick={storeModal.onClose}>Cancel</Button>
-                                <Button disabled={isLoading} type="submit">Create Store</Button>
-                            </div>
-                        </form>
-                    </Form>
-                </div>
-            </div>
-        </Modal>
-    )
-} 
+  return (
+    <Modal
+      title="Create Store"
+      desc="New products and categories! Nice one baby girl"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Store Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        placeholder="Shirts Store"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button
+                  disabled={isLoading}
+                  variant={"outline"}
+                  onClick={storeModal.onClose}
+                >
+                  Cancel
+                </Button>
+                <Button disabled={isLoading} type="submit">
+                  Create Store
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  )
+}
