c16027a3607445bef4366a5a08bc2786c07101c7
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-09-21 09:27:00 +0100

    Store create feature complete

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,20 @@
+import { UserButton } from "@clerk/nextjs";
+import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+
 const Navbar = () => {
 	return ( 
-		<div>This is a navbar</div>
+		<div className="border-b">
+			<div className="flex h-16 items-center px-4">
+				<div>
+					<StoreSwitcher />
+				</div>
+				<MainNav className="mx-6"/>
+				<div className="ml-auto flex items-center space-x-4">
+					<UserButton afterSignOutUrl="/" showName />
+				</div>
+			</div>
+		</div>
 	 );
 }
  
