d7a20a3994d118985f9ecf1d7400227f494e89d6
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-09-24 15:09:11 +0100

    Setup billboards router

--- components/ui/api-list.tsx
+++ components/ui/api-list.tsx
@@ -1,7 +1,34 @@
-export const ApiList = () => {
+'use client'
+
+import { useOrigin } from "@/hooks/use-origin";
+import { useParams } from "next/navigation";
+import { APIAlert } from "./api-alert";
+
+interface APIListProps {
+	entityName: string
+	entityIdName: string
+}
+
+
+export const ApiList: React.FC<APIListProps> = (
+	{
+		entityName,
+		entityIdName
+	}
+) => {
+	const params = useParams();
+	const origin = useOrigin();
+
+
+	const baseURL = `${origin}/api/${params.storeId}`;
+
 	  return (
-	<div>
-	  <h1>ApiList</h1>
-	</div>
+	<>
+		<APIAlert 
+			title="GET" 
+			variant="public"
+			desc={`${baseURL}/${entityName}`}
+		/>
+	</>
   )
 }
