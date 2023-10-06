bb2ddbf4dc1cf1390b094595124dfd42d376312b
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-10-06 13:33:22 +0100

    Setup dashboard overview && Finished stripe webhook route

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,5 @@
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb";
 
 
@@ -16,7 +18,14 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
 		}
 	})
 	return ( 
-		<div>
+		<div className="flex-col">
+			<div className="flex-1 space-y-4 p-8 pt-6">
+				<Heading title="Dashboard" description="Overview of your store"/>
+				<Separator />
+				<div className="grid gap-4 grid-cols-3">
+
+				</div>
+			</div>
 			Active Store: {store?.name}
 		</div>
 	 );
