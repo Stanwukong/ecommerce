7d00b5b456754301d66f65196630c3dc9cafc282
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-10-06 15:58:36 +0100

    Added theme switcher

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 const Navbar = async () => {
 	const { userId } = auth();
@@ -25,7 +26,8 @@ const Navbar = async () => {
 				</div>
 				<MainNav className="mx-6"/>
 				<div className="ml-auto flex items-center space-x-4">
-					<UserButton afterSignOutUrl="/" showName />
+					<ThemeToggle />
+					<UserButton afterSignOutUrl="/" />
 				</div>
 			</div>
 		</div>
--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -10,7 +10,7 @@ export const Heading: React.FC<HeadingProps> = ({
 	return ( 
 		<div className="flex items-center justify-between">
 			<div>
-				<h1 className="text-3xl font-bold text-gray-900">{title}</h1>
+				<h1 className="text-3xl font-bold text-gray-900 dark:text-white">{title}</h1>
 				<p className="text-sm font-medium text-gray-500">{description}</p>
 			</div>
 		</div>
